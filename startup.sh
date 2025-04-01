3719fcb3460b5cc95442d9ed76028a96ce95a853
Edward KIM <fharenheit@gmail.com>
2025-04-02 02:32:04 +0900

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#
+# Author: KIM BYOUNGGON(architect@data-dynamics.io)
+# Description: Python 애플리케이션을 백그라운드에서 실행하고 PID를 저장하는 스크립트
+# Usage: sh startup.sh
+#
 
 APP_NAME="server.py"
 CONFIG="config.yaml"
